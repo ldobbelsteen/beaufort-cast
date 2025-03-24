1b0682a6d8c062b7de46a9e52ea4c92b4352e4e5
Lukas Dobbelsteen <l.dobbelsteen@gmail.com>
2025-03-24 15:47:50 +0100

    Rewrite for Immich

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
-import mimetypes
-import random
-import sys
-import threading
+import math
 import os
 import logging
-import time
-import uuid
-from datetime import datetime
-from bottle import static_file, request, abort, run, route
 import pychromecast
-
-PHOTO_DIR = os.environ["PHOTO_DIR"]
-CAST_DEVICE_NAME = os.environ["CAST_DEVICE_NAME"]
-LOCAL_IP = os.environ["LOCAL_IP"]
-LISTENING_PORT = int(os.environ["LISTENING_PORT"])
-BLACKLISTED_DIR_NAMES = os.environ["BLACKLISTED_DIR_NAMES"].split(";")
-CAST_CHECK_INTERVAL_SECS = float(os.environ["CAST_CHECK_INTERVAL_SECS"])
-NEXT_PHOTO_INTERVAL_SECS = float(os.environ["NEXT_PHOTO_INTERVAL_SECS"])
-PHOTO_INDEX_INTERVAL_SECS = float(os.environ["PHOTO_INDEX_INTERVAL_SECS"])
+import time
+import requests
+import random
+from datetime import datetime
+import re
+from cachetools import LRUCache, cached, TTLCache
 
 
 def year_progress():
@@ -30,212 +20,202 @@ def year_progress():
     return seconds_elapsed / total_seconds_in_year
 
 
-def is_not_blacklisted(dir_name: str) -> bool:
-    return all(
-        banned.lower() not in dir_name.lower() for banned in BLACKLISTED_DIR_NAMES
+def is_blacklisted(name: str, substr_blacklist: frozenset[str]) -> bool:
+    """Returns whether the given name contains blacklisted substrings."""
+    return any(banned.lower() in name.lower() for banned in substr_blacklist)
+
+
+def year_from_album_name(name: str) -> int | None:
+    """Extracts the year from the given album name."""
+    match = re.fullmatch(r"\[(\d{4})[\]-].*", name)
+    if match is None:
+        return None
+    return int(match.group(1))
+
+
+@cached(cache=TTLCache(maxsize=1, ttl=7200))
+def list_albums_by_year(
+    immich_base_url: str,
+    immich_api_key: str,
+    album_substr_blacklist: frozenset[str],
+):
+    """List all album IDs and group them by year sorted increasingly."""
+    resp = requests.get(
+        f"{immich_base_url}/api/albums",
+        headers={
+            "Accept": "application/json",
+            "x-api-key": immich_api_key,
+        },
     )
+    resp.raise_for_status()
+    albums = resp.json()
+
+    by_year: dict[int, list[str]] = {}
+    for album in albums:
+        id, name = album["id"], album["albumName"]
+        assert isinstance(id, str)
+        assert isinstance(name, str)
+
+        if is_blacklisted(name, album_substr_blacklist):
+            logging.info(f"ignoring blacklisted album '{name}'")
+            continue
+
+        year = year_from_album_name(name)
+        if year is None:
+            logging.warning(
+                f"ignoring album for which year cannot be extracted '{name}'"
+            )
+            continue
+
+        if year not in by_year:
+            by_year[year] = []
+        by_year[year].append(id)
+
+    return [by_year[year] for year in sorted(by_year.keys())]
 
 
-def collect_photos_recursively(dir: str):
-    result: list[tuple[str, str]] = []
-    for entry in os.scandir(dir):
-        if entry.is_file():
-            content_type, _ = mimetypes.guess_type(entry.name)
-            if content_type is not None:
-                if content_type in [
-                    "image/apng",
-                    "image/bmp",
-                    "image/gif",
-                    "image/jpeg",
-                    "image/png",
-                    "image/webp",
-                ]:  # based on https://developers.google.com/cast/docs/media
-                    result.append((entry.path, content_type))
-                else:
-                    logging.debug(
-                        f"ignoring file '{entry.path}' that is likely not an image or has unsupported format"
-                    )
-            else:
-                logging.debug(
-                    f"ignoring file '{entry.path}' for which content type could not be determined"
-                )
-        elif entry.is_dir():
-            if is_not_blacklisted(entry.name):
-                result.extend(collect_photos_recursively(entry.path))
-            else:
-                logging.debug(f"ignoring blacklisted directory '{entry.path}'")
+@cached(cache=LRUCache(maxsize=1))
+def compute_year_weights(year_count: int, year_decay_factor: float) -> list[float]:
+    assert year_decay_factor >= 1.0
+
+    # The weights grow exponentially (by the decay factor) with the year.
+    result = [year_decay_factor**i for i in range(year_count)]
+
+    # The last year should be weighted by the progress through the year.
+    result[-1] = year_progress() * result[-1]
+
     return result
 
 
-class PhotoIndex:
-    # First layer represents years, second layer represents subdirectories within years,
-    # third layer represents photos within subdirectories.
-    year_photos: list[list[list[tuple[str, str]]]]
-
-    # Weights for each year, used for random selection.
-    year_weights: list[float]
-
-    # The time this index was created.
-    time: datetime
-
-    def __init__(self):
-        logging.info("indexing photos...")
-
-        year_entries: list[os.DirEntry[str]] = []
-        for entry in os.scandir(PHOTO_DIR):
-            if entry.is_dir():
-                if entry.name.isnumeric():
-                    if is_not_blacklisted(entry.name):
-                        year_entries.append(entry)
-                    else:
-                        logging.debug(
-                            f"ignoring blacklisted year directory '{entry.path}'"
-                        )
-                else:
-                    logging.warning(
-                        f"ignoring non-year directory found in root of photo directory '{entry.path}'"
-                    )
-            else:
-                logging.warning(
-                    f"ignoring non-directory found in root of photo directory '{entry.path}'"
-                )
-
-        year_entries.sort(key=lambda x: x.name)
-        year_photos: list[list[list[tuple[str, str]]]] = []
-
-        for entry in year_entries:
-            subdir_entries: list[os.DirEntry[str]] = []
-            for subentry in os.scandir(entry.path):
-                if subentry.is_dir():
-                    if is_not_blacklisted(subentry.name):
-                        subdir_entries.append(subentry)
-                    else:
-                        logging.debug(
-                            f"ignoring blacklisted year subdirectory '{subentry.path}'"
-                        )
-                else:
-                    logging.warning(
-                        f"ignoring non-directory found in year subdirectory '{subentry.path}'"
-                    )
-
-            subdir_entries.sort(key=lambda x: x.name)
-            subdir_photos = []
-
-            for subentry in subdir_entries:
-                photos = collect_photos_recursively(subentry.path)
-                if len(photos) > 0:
-                    subdir_photos.append(
-                        [
-                            (os.path.relpath(path, PHOTO_DIR), content_type)
-                            for path, content_type in photos
-                        ]
-                    )
-
-            year_photos.append(subdir_photos)
-
-        logging.info("indexing complete")
-
-        self.year_photos = year_photos
-
-        # First year gets 1, second 2, third 4, fourth 8, fifth 16, etc.
-        self.year_weights = [2**i for i in range(len(year_photos))]
-
-        # If there are photos for the current year, make the weight for the current year
-        # proportional to the progress through the year to prevent the same photos from being
-        # shown too often.
-        latest_year = int(year_entries[-1].name)
-        current_year = datetime.now().year
-        if latest_year == current_year:
-            self.year_weights[-1] = int(round(year_progress() * self.year_weights[-1]))
-
-        self.time = datetime.now()
-
-    def get_random_photo(self) -> tuple[str, str]:
-        year = random.choices(self.year_photos, weights=self.year_weights, k=1)[0]
-        subdir = random.choice(year)
-        return random.choice(subdir)
-
-    def is_outdated(self) -> bool:
-        return (datetime.now() - self.time).total_seconds() > PHOTO_INDEX_INTERVAL_SECS
+def is_supported_image_format_on_chromecast(content_type: str) -> bool:
+    return content_type in [
+        "image/apng",
+        "image/bmp",
+        "image/gif",
+        "image/jpeg",
+        "image/png",
+        "image/webp",
+    ]  # based on https://developers.google.com/cast/docs/media#image_formats
 
 
-def run_photo_server(key: str):
-    """Spawn a Bottle server to serve photos."""
+@cached(cache=TTLCache(maxsize=math.inf, ttl=7200))
+def list_album_assets(
+    immich_base_url: str,
+    immich_api_key: str,
+    album_id: str,
+) -> list[tuple[str, str]]:
+    """List all asset IDs and content types in the given album."""
+    resp = requests.get(
+        f"{immich_base_url}/api/albums/{album_id}",
+        headers={
+            "Accept": "application/json",
+            "x-api-key": immich_api_key,
+        },
+    )
+    resp.raise_for_status()
+    album = resp.json()
 
-    @route("/<path:path>")
-    def send_photo(path: str):
-        if request.query.key == key:
-            return static_file(path, PHOTO_DIR)
-        else:
-            abort(401, "unauthorized")
+    result = []
+    for asset in album["assets"]:
+        id, content_type = asset["id"], asset["originalMimeType"]
+        assert isinstance(id, str)
+        assert isinstance(content_type, str)
 
-    run(
-        host=LOCAL_IP,
-        port=LISTENING_PORT,
-        quiet=True,
+        if is_supported_image_format_on_chromecast(content_type):
+            result.append((id, content_type))
+
+    return result
+
+
+def direct_asset_url(base_url: str, api_key: str, id: str) -> str:
+    return f"{base_url}/api/assets/{id}/original?apiKey={api_key}"
+
+
+def pick_random_photo(
+    immich_base_url: str,
+    immich_api_key: str,
+    album_substr_blacklist: frozenset[str],
+    year_decay_factor: float,
+) -> tuple[str, str]:
+    albums_by_year = list_albums_by_year(
+        immich_base_url,
+        immich_api_key,
+        album_substr_blacklist,
     )
 
+    year_weights = compute_year_weights(len(albums_by_year), year_decay_factor)
+    year = random.choices(albums_by_year, weights=year_weights, k=1)[0]
+    album = random.choice(year)
 
-def main():
-    # Create random key for image server to prevent unauthorized access.
-    key = str(uuid.uuid4())
-    logging.debug(f"key: {key}")
+    assets = list_album_assets(immich_base_url, immich_api_key, album)
+    return random.choice(assets)
 
-    # Start the image server.
-    thread = threading.Thread(target=run_photo_server, args=[key])
-    thread.daemon = True
-    thread.start()
 
-    index = PhotoIndex()
-
-    def path_to_url(path: str) -> str:
-        return f"http://{LOCAL_IP}:{LISTENING_PORT}/{path}?key={key}"
-
-    casts, _ = pychromecast.get_listed_chromecasts(friendly_names=[CAST_DEVICE_NAME])
+def get_chromecast(name: str) -> pychromecast.Chromecast:
+    casts, _ = pychromecast.get_listed_chromecasts(friendly_names=[name])
     if len(casts) == 0:
-        logging.error(f"chromecast '{CAST_DEVICE_NAME}' not found")
-        sys.exit(1)
+        raise Exception(f"chromecast '{name}' not found")
     elif len(casts) > 1:
-        logging.error(f"multiple chromecasts found with name '{CAST_DEVICE_NAME}'")
-        sys.exit(1)
-    cast = casts[0]
+        raise Exception(f"multiple chromecasts found with name '{name}'")
+    return casts[0]
+
+
+def main(
+    chromecast_name: str,
+    immich_base_url: str,
+    immich_api_key: str,
+    album_substr_blacklist: frozenset[str],
+    year_decay_factor: float,
+    photo_interval_secs: int,
+):
+    cast = get_chromecast(chromecast_name)
+    cast_backoff_secs = 27
 
     while True:
-        if index.is_outdated():
-            index = PhotoIndex()
+        # Request the status of the Chromecast.
         cast.wait()
 
-        # We *should* now have the status.
-        if cast.status is not None:
-            # If the cast is idle, start casting.
-            if (
-                cast.status.display_name
-                == "Backdrop"  # default chromecast ambient mode, which we can override
-                or cast.status.display_name
-                == "Default Media Receiver"  # continue previous session (in case of this script crashing or restarting)
-            ):
-                logging.info("casting...")
-                mc = cast.media_controller
-
-                # Cast photos while we still have control.
-                while True:
-                    if index.is_outdated():
-                        index = PhotoIndex()
-
-                    path, content_type = index.get_random_photo()
-                    mc.play_media(path_to_url(path), content_type)
-
-                    time.sleep(NEXT_PHOTO_INTERVAL_SECS)
-                    if cast.status.display_name != "Default Media Receiver":
-                        break
-
-                logging.info("finished casting")
-            else:
-                logging.debug("chromecast not idle")
-        else:
+        # Check whether the request was successful.
+        if cast.status is None:
             logging.warning("chromecast status unknown")
+            time.sleep(cast_backoff_secs)
+            continue
 
-        time.sleep(CAST_CHECK_INTERVAL_SECS)
+        # Check whether the Chromecast is idle.
+        if (
+            cast.status.display_name
+            not in [
+                "Backdrop",  # default chromecast ambient mode, which we can override
+                "Default Media Receiver",  # continue previous session (in case of this script crashing or restarting)
+            ]
+        ):
+            logging.debug(f"chromecast not idle, status: {cast.status.display_name}")
+            time.sleep(cast_backoff_secs)
+            continue
+
+        # Start a casting session on the Chromecast.
+        logging.info("casting...")
+        mc = cast.media_controller
+
+        # Cast photos while we still have control.
+        while True:
+            id, content_type = pick_random_photo(
+                immich_base_url,
+                immich_api_key,
+                album_substr_blacklist,
+                year_decay_factor,
+            )
+            url = direct_asset_url(immich_base_url, immich_api_key, id)
+            logging.debug(f"casting photo {id} ({content_type})")
+            mc.play_media(url, content_type)
+            time.sleep(photo_interval_secs)
+
+            # Stop if we lost control of the cast.
+            if cast.status.display_name != "Default Media Receiver":
+                break
+
+        logging.info("finished casting")
+        time.sleep(cast_backoff_secs)
 
 
 if __name__ == "__main__":
@@ -244,4 +224,11 @@ if __name__ == "__main__":
         format="%(asctime)s %(levelname)s: %(message)s",
     )
 
-    main()
+    main(
+        os.environ["CHROMECAST_NAME"],
+        os.environ["IMMICH_BASE_URL"],
+        os.environ["IMMICH_API_KEY"],
+        frozenset(os.environ["ALBUM_SUBSTR_BLACKLIST"].split(";")),
+        float(os.environ["YEAR_DECAY_FACTOR"]),
+        int(os.environ["PHOTO_INTERVAL_SECS"]),
+    )
